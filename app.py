d95c076dac4ecc4cb88243105330bcc8c3711e2b
siddharth kumar <47270817+siddhhu@users.noreply.github.com>
2021-11-08 20:21:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,session, redirect, url_for, flash
+from flask import Flask, render_template,session, redirect, url_for, flash,request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField
